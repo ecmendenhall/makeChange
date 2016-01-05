e793e5fdb7dd3a6374101a83b592c2ecc5aadb56
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-05 15:29:03 -0500

    add logic to pass testNineCents

--- src/com/bjorn/MakeChange/MakeChange.java
+++ src/com/bjorn/MakeChange/MakeChange.java
@@ -9,25 +9,30 @@ public class MakeChange {
 
     public HashMap<Character, Integer> makeChange(int cashTendered) {
         HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
+        int temp = 0;
 
-//        if(cashTendered == 0) {
-//            return hmap;
-//        }
         while (cashTendered > 0) {
             if(cashTendered == 0){
                 return hmap;
             }
-            else if(cashTendered < 5){
-                int temp = 0;
+
+            if (cashTendered > 5){
+                temp = 0;
+                if (hmap.get('N') != null) {
+                    temp = hmap.get('N');
+                }
+                hmap.put('N', temp + 1);
+                cashTendered -= 5;
+            }
+            else {
+                temp = 0;
                 if (hmap.get('P') != null) {
                     temp = hmap.get('P');
                 }
                 hmap.put('P', temp + 1);
                 cashTendered--;
             }
-            else {
-                return hmap;
-            }
+
 
         }
         return hmap;
