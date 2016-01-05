6407bc4b7b552abef273b3e32c99f3c4e853a31d
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-05 14:08:02 -0500

    add zeroCent test/code and oneCent failing test

---
+++ src/com/bjorn/MakeChange/MakeChange.java
@@ -0,0 +1,17 @@
+package com.bjorn.MakeChange;
+
+import java.util.HashMap;
+
+/**
+ * Created by bjornjohnson on 1/5/16.
+ */
+public class MakeChange {
+
+    public HashMap<Character, Integer> makeChange(int cashTendered) {
+        HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
+
+        
+
+        return hmap;
+    }
+}
---
+++ test/com/bjorn/MakeChange/test/MakeChangeTest.java
@@ -0,0 +1,45 @@
+package com.bjorn.MakeChange.test;
+
+import com.bjorn.MakeChange.MakeChange;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by bjornjohnson on 1/5/16.
+ */
+public class MakeChangeTest {
+
+    private MakeChange makeChange;
+    private HashMap<Character, Integer> hmap;
+
+    @Before
+    public void setUp() {
+        makeChange = new MakeChange();
+        hmap = new HashMap<Character, Integer>();
+    }
+
+
+    @Test
+    public void testInstanceOf() {
+        assertThat(makeChange, instanceOf(MakeChange.class));
+    }
+
+    @Test
+    public void testZeroCurrency() {
+        assertEquals(hmap, makeChange.makeChange(0));
+    }
+
+    @Test
+    public void testOneCent() {
+        hmap.put('P', 1);
+        assertEquals(hmap, makeChange.makeChange(1));
+    }
+
+
+}
