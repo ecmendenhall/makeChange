a46c2df1c5c25afcaa2f39ec3979ddcc1139b130
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-06 12:55:48 -0500

    change name of update hash function to be more descriptive

--- src/com/bjorn/MakeChange/IMakeChange.java
+++ src/com/bjorn/MakeChange/IMakeChange.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
  * Created by bjornjohnson on 1/6/16.
  */
 public interface IMakeChange {
-    void updateHash(String cointType, HashMap<String, Integer> hmap);
+    void updateCoinAmountHashMap(String cointType, HashMap<String, Integer> hmap);
 
     HashMap<String, Integer> makeChange(int cashTendered);
 }
