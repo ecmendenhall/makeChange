c9bfb36b23fe42c73017599edbebe81ec25f09f6
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-18 16:48:46 -0500

    remove intellij attribution comment

--- src/com/bjorn/MakeChange/IMakeChange.java
+++ src/com/bjorn/MakeChange/IMakeChange.java
@@ -2,9 +2,6 @@ package com.bjorn.MakeChange;
 
 import java.util.HashMap;
 
-/**
- * Created by bjornjohnson on 1/6/16.
- */
 public interface IMakeChange {
 
     HashMap<String, Integer> makeChange(int cashTendered);
